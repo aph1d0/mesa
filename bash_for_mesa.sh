5338739c1ae92932bd82cb0296b295d2a7937e15
root <root@DESKTOP-8POCI91.localdomain>
2020-08-10 22:15:49 +0200

    2.0\_0.01

--- bash_for_mesa.sh
+++ bash_for_mesa.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-initial_mass=2.4
+initial_mass=2.0
 overshootsf=( 0.01 0.02 0.03)
 sed -i "s/initial_mass =.*/initial_mass = $initial_mass/"  inlist_ZAMStoWD
 for i in ${overshootsf[*]}
@@ -9,7 +9,6 @@ do
 	git add .
 	git commit -m "$initial_mass\_$i"
 	git push
-	curl http://35.240.16.76/git/notifyCommit?url=https://github.com/aph1d0/mesa.git
 	sleep 600
 done
 
