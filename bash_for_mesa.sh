e86d5e9c61ca25d4a861fbe7de5b039640279f60
root <root@DESKTOP-8POCI91.localdomain>
2020-08-11 22:58:55 +0200

    2.0'_'0.07

--- bash_for_mesa.sh
+++ bash_for_mesa.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 initial_mass=2.0
-overshootsf=( 0.04 0.05 0.06 )
+overshootsf=( 0.07 0.08 0.09 )
 sed -i "s/initial_mass =.*/initial_mass = $initial_mass/"  inlist_ZAMStoWD
 for i in ${overshootsf[*]}
 do 
 	sed -i "s/overshoot_f(1) =.*/overshoot_f(1) = $i/"  inlist_ZAMStoWD
 	sed -i "s/overshoot_f0(1) =.*/overshoot_f0(1) = $(python -c "print($i*0.2)")/"  inlist_ZAMStoWD
 	git add .
-	git commit -m "$initial_mass\_$i"
+	git commit -m "$initial_mass'_'$i"
 	git push
 	sleep 600
 done
